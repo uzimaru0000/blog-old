0a2470d220621fc73800c22ae612f95c6e0bfe20
ShujiOba <shuji365630@gmail.com>
2020-01-14 15:49:08 +0900

    旧srcを削除

--- src/api/server.ts
+++
@@ -1,9 +0,0 @@
-import * as API from './api';
-import fetch from 'node-fetch';
-
-const endpoint = 'https://blog.uzimaru.com/api';
-
-export const getEntries = () => API.getEntries(fetch(`${endpoint}/entries`));
-
-export const getEntry = (id: string) =>
-  API.getEntry(fetch(`${endpoint}/entry/${id}`));
